4ea6c519278ed37de005b1fcfdc755159ec385fb
RootRice <michaelhindle95@gmail.com>
2022-05-25 16:19:19 +0100

    Changed some textures and added moving platform

--- ItsyBitsySpider/Assets/Programming/Scripts/MovingPlatform.cs
+++ ItsyBitsySpider/Assets/Programming/Scripts/MovingPlatform.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] Vector2[] patrolPoints;
+    [SerializeField] Transform snailTransform;
+    [SerializeField] float speed;
+
+    int curTarget = 1;
+    private void Start()
+    {
+        snailTransform.localPosition = patrolPoints[0];
+    }
     private void Update()
     {
         
+        snailTransform.localPosition = Vector2.MoveTowards(snailTransform.localPosition, patrolPoints[curTarget], speed * Time.deltaTime);
+        float angle = Mathf.Atan2(patrolPoints[curTarget].y - snailTransform.localPosition.y, patrolPoints[curTarget].x - snailTransform.localPosition.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+        snailTransform.rotation = Quaternion.RotateTowards(snailTransform.rotation, targetRotation, 40*speed * Time.deltaTime);
+        if (new Vector2(snailTransform.transform.localPosition.x, snailTransform.transform.localPosition.y) == patrolPoints[curTarget])
+        {
+            curTarget += 1;
+            if(curTarget == patrolPoints.Length)
+            {
+                curTarget = 0;
+            }
+        }
     }
 
     private void OnDrawGizmosSelected()
